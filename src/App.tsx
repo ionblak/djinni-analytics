6e0d1f8efbfbf48137ff0d4a286d3a2b7c9e5bbd
Ionaigor10 <ionaigor10@gmail.com>
2022-10-09 16:21:47 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './pages/home'
+import { NotFound } from './pages/notFound'
 
 export const App: FC = () => {
   return (
-    <div className="App">
-      Home
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
